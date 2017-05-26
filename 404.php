79e4c2c9b828c63a091e55d2071537d654fbdcf8
Patrick Best <patrickbest@patrickbest.com>
2017-05-26 17:45:58 +0100

    scaffold 404

--- 404.php
+++ 404.php
@@ -2,10 +2,19 @@
 get_header();
 ?>
 
-<section id="main-content">
-  <h1>Not found</h1>
-  <p>:(</p>
-</section>
+<main id="main-content">
+  <section id="posts">
+    <div class="container">
+      <div id="masonry-container" class="grid-row">
+
+      <article class="grid-item item-s-12 item-m-6 item-l-4 margin-bottom-small text-align-center">
+        <?php _e('Sorry nothing found'); ?>
+      </article>
+
+    </div>
+  </section>
+
+</main>
 
 <?php
 get_footer();
