b1f893cad375c9d7f5e92d265bad8d3f8b5efcec
Patrick Best <patrickbest@patrickbest.com>
2017-05-29 14:54:17 +0100

    fix form typo

--- partials/contact-form.php
+++ partials/contact-form.php
@@ -4,7 +4,7 @@
 
 <form class="contact-form">
   <h3 class="margin-bottom-small">Send an enquiry about <?php echo $post->post_title; ?>:</h3>
-  <input type="hidden" name="project" id="product" value="<?php echo $post->post_title; ?>">
+  <input type="hidden" name="product" id="product" value="<?php echo $post->post_title; ?>">
   <input type="hidden" name="nonce" id="nonce" value="<?php echo wp_create_nonce('enquiry'); ?>">
   <label for="from">From:</label>
   <input type="email" name="from" id="from" placeholder="youremail@domain.com"/>
